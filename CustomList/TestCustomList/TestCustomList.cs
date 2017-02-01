97fd49c24cc3a5d1ea0b7b720fd998805108b15f
battlequinn <quinnthebattle@gmail.com>
2017-02-01 12:27:26 -0600

    Created Test program. Created Run and CustomList classes. Set up program class to run input.

---
+++ CustomList/TestCustomList/TestCustomList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCustomList
+{
+    [TestClass]
+    public class TestCustomList
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
